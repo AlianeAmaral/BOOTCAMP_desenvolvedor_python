637a3ee2d9e15771a4008b8803ca09dbf439038b
Aliane E. Amaral <108065159+AlianeAmaral@users.noreply.github.com>
2023-03-08 05:15:04 -0400

    Update capitulo1_aula4.py

--- capitulo1_aula4.py
+++ capitulo1_aula4.py
@@ -3,3 +3,4 @@ print("Olá", nome, "! Seja bem-vindo(a) !")
  
 //não necessariamente é necessário escrever "print" quando se trata de operação, pois o google colab já imprime o resultado da última operação realizada.
 //no google colab os arquivos podem ser gerados como .ipynb 
+ 
